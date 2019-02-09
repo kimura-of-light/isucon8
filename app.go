df84a61ac26a74881d72c942fe41c94bc9d7677d
kamono-mafuyu <kamono-mafuyu@dmm.com>
2019-02-09 19:58:51 +0900

    Added log

--- app.go
+++ app.go
@@ -25,6 +25,18 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
+func init() {
+	file, err := os.OpenFile("torb-app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		//エラー処理
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	log.SetOutput(io.MultiWriter(file, os.Stdout))
+	log.SetFlags(log.Ldate | log.Ltime)
+}
+
 type User struct {
 	ID        int64  `json:"id,omitempty"`
 	Nickname  string `json:"nickname,omitempty"`
