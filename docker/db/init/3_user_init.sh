60046e7d819978018f1129fd333954cea25d8504
nakamura-kyota <nakamura-kyota@dmm.com>
2019-02-16 18:10:16 +0900

    add my.cnf

--- docker/db/init/3_user_init.sh
+++ docker/db/init/3_user_init.sh
@@ -5,3 +5,4 @@ CREATE USER 'isucon'@'%' IDENTIFIED BY 'isucon';
 GRANT ALL ON torb.* TO 'isucon'@'%';
 CREATE USER 'isucon'@'localhost' IDENTIFIED BY 'isucon';
 GRANT ALL ON torb.* TO 'isucon'@'localhost';
+EOF
