a7f96fb89e2a2d49501cdb7175d299bb7a1e3dae
nakamura-kyota <nakamura-kyota@dmm.com>
2019-02-16 17:03:31 +0900

    add user init

---
+++ docker/db/init/3_user_init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cat <<'EOF' | mysql -uroot
+CREATE USER 'isucon'@'%' IDENTIFIED BY 'isucon';
+GRANT ALL ON torb.* TO 'isucon'@'%';
+CREATE USER 'isucon'@'localhost' IDENTIFIED BY 'isucon';
+GRANT ALL ON torb.* TO 'isucon'@'localhost';
